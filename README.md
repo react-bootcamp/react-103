b2b63d5e911f7319da82466f83bb0b35b1ec7b8f
Mathieu <mathieu.ancelin@gmail.com>
2016-11-02 12:31:04 +0100

    First import

---
+++ README.md
@@ -0,0 +1,17 @@
+# React Wines 102
+
+This is the result of the [React 102 Workshop](https://github.com/react-bootcamp/react-102)
+
+## Run
+
+```sh
+npm install
+npm start
+```
+
+or if you use `yarn`
+
+```sh
+yarn install
+yarn run start
+```
---
+++ src/components/WineApp.js
@@ -0,0 +1,46 @@
+import React, { PropTypes } from 'react';
+
+export const WineApp = React.createClass({
+  contextTypes: {
+    router: PropTypes.object
+  },
+  goBack(e) {
+    e.preventDefault();
+    this.context.router.goBack();
+  },
+  goHome(e) {
+    e.preventDefault();
+    const root = window.location.hostname === 'react-bootcamp.github.io'
+      ? '/react-wines-103/'
+      : '/';
+    this.context.router.push({
+      pathname: `${root}`
+    });
+  },
+  render() {
+    const displayButton = window.location.pathname === '/' || window.location.pathname === '/react-wines-102/'
+      ? false
+      : true;
+    return (
+      <div className="container">
+        <h1 className="center-align">Open Wine Database</h1>
+        <div className="center-align">
+          You can read the Wines API documentation at <a href="https://bit.ly/rbw-api" target="_blank">https://wines-api.herokuapp.com</a> and try it <a href="https://bit.ly/rbw-api-swag" target="_blank">here</a>
+        </div>
+        {displayButton && (<div className="center-align" style={{ marginTop: 20 }}>
+          <button className="btn waves-effect waves-light" onClick={this.goBack} type="button">
+            <i className="material-icons left">fast_rewind</i>
+            Back
+          </button>
+          <button className="btn waves-effect waves-light" style={{ marginLeft: 10 }} onClick={this.goHome} type="button">
+            <i className="material-icons left">home</i>
+            Home
+          </button>
+        </div>)}
+        <div className="row">
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+});
